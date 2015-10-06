69a0c7d529559c9ca66dee4a2b78daa073f372bb
inikulin <ifaaan@gmail.com>
2015-10-06 22:35:35 +0300

    Expose `toAbsPath`. Add support for calls from native methods. Bump version.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
-var assert   = require('assert');
-var read     = require('../').read;
-var readSync = require('../').readSync;
+var assert    = require('assert');
+var path      = require('path');
+var read      = require('../').read;
+var readSync  = require('../').readSync;
+var toAbsPath = require('../').toAbsPath;
 
 it('Should read file sync', function () {
     assert.strictEqual(readSync('./data/file1').trim(), '42');
@@ -26,4 +28,31 @@ it('Should read file async with options', function (done) {
         assert.strictEqual(content.trim(), '42');
         done();
     });
+});
+
+it('Should provide conversion to absolute path', function () {
+    assert.strictEqual(toAbsPath('./data/file1'), path.join(__dirname, './data/file1'));
+});
+
+it('Should provide support for Array native methods', function () {
+    var files = [
+        './data/file1',
+        './data/file2'
+    ];
+
+    var actualPaths = files.map(toAbsPath);
+
+    var expectedPaths = files.map(function (file) {
+        return path.join(__dirname, file);
+    });
+
+    assert.deepEqual(actualPaths, expectedPaths);
+
+    var actualContent = files.map(function (file) {
+        return readSync(file).trim();
+    });
+
+    var expectedContent = ['42', 'Hey ya!'];
+
+    assert.deepEqual(actualContent, expectedContent);
 });
